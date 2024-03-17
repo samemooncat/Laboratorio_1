525548e6f8291150793c327b3a0e07f07421cd6f
NicoValVilla <162645698+NicoValVilla@users.noreply.github.com>
2024-03-16 23:38:57 -0500

    final changes

--- README.md
+++ README.md
@@ -1,34 +1,9 @@
 # LABORATORIO_1
 
-# ¿QUE ES UN WIDGET?
-    Los widgets son microaplicaciones que se despliegan en la pantalla de tu computador, en una página web o en tu dispositivo móvil.
+#¿QUE ES UN WIDGET?
+    #Los widgets son microaplicaciones que se despliegan en la pantalla de tu computador, en una página web o en tu dispositivo móvil.
     #Incluso en la misma aplicación los componentes también pueden ser widgets.
 
-# Widgets Identificados
-    - MaterialApp: Es el widget principal que configura la aplicación Flutter. Define aspectos globales de la aplicación, como el tema y la pantalla inicial.
-
-    - ChangeNotifierProvider: Es un widget proporcionado por el paquete provider que permite proporcionar un objeto ChangeNotifier a una parte de la interfaz de usuario de su aplicación. En este caso, proporciona una instancia de MyAppState a la aplicación.
-
-    - Scaffold: Es un widget que proporciona la estructura básica de la página de su aplicación, como el fondo, la barra de aplicaciones, el cajón de navegación, etc.
-
-    - LayoutBuilder: Es un widget que construye su hijo mediante una función que le permite saber las restricciones de diseño actuales.
-
-    - NavigationRail: Es un widget de navegación que muestra las opciones de navegación en un carril lateral. Es útil para aplicaciones con múltiples secciones navegables.
-
-    - Row: Un widget que organiza a sus hijos en una fila horizontal.
-
-    - SafeArea: Un widget que asegura que su hijo no se coloque debajo de los elementos de la barra de estado y la barra de navegación.
-
-    - Column: Un widget que organiza a sus hijos en una columna vertical.
-
-    - ElevatedButton: Un widget que implementa un botón con una elevación para dar una sensación táctil.
-
-    - SizedBox: Un widget que proporciona un espacio fijo en un eje específico.
-
-    - Card: Un widget que muestra su hijo en una tarjeta con bordes redondeados y sombras.
-
-    - Text: Un widget que muestra texto con estilo.
-
-    - ListView: Un widget que muestra una lista de hijos en una vista de desplazamiento vertical.
-
-    - ListTile: Un widget que proporciona una fila con un título y un subtitulo opcional, a menudo utilizado en listas.
+#Widgets Identificados
+    #MyHomePage
+    #
--- lib/main.dart
+++ lib/main.dart
@@ -2,212 +2,96 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+
+void main () {
+runApp(Lab());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
+class Lab extends StatelessWidget{
+const Lab ({super.key});
+@override
+Widget build(BuildContext context){
+return ChangeNotifierProvider(
+create: (context) =>LabState(),
+child: MaterialApp(
+title: 'CampusMatch',
+theme: ThemeData(
+useMaterial3: true,
+colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+),
+home: MyHomePage(),
+),
+);
+}
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class LabState extends ChangeNotifier {
+var current = WordPair.random();
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
+void getNext(){ //este metodo de la una nueva palabra aleatoria a current
+current= WordPair.random();
+notifyListeners();
+}
 }
 
-// ...
+class MyHomePage extends StatelessWidget {
+@override
+Widget build(BuildContext context){
+var appState = context.watch();
+var pair = appState.current;
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+return Scaffold(
+body: Center( //; se puede centrar dandole click derecho a column, refactor y wrap with center
+child: Column( //widget que ordena en forma lista los widget hijos debajo de hijos;
+mainAxisAlignment: MainAxisAlignment.center, //centra los elementos dentro de column pero a lo largo de el eje principal (vertical)
+children: [
+//const Text('este es tu boton de confianza:'),
+BigCard(pair: pair), //esto crea una nueva clase de la tarjeta
+//Text(appState.current.asUpperCase), // asUpperCase: metodo para que las palabals esten en mayuscula, se puede cambiar
+SizedBox(height: 10),
+
+
+ElevatedButton(
+onPressedtriste){
+appState.getNext(); //llamamos el metodo cada vez que presionamos el boton
+print('button pressed!'); // en la consola aparece este mensaje cada vez que oprimimos el boton
+},
+child: Text('next'),
+)
+],
+),
+)
+);
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
 }
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// ...
 
 class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+const BigCard({
+super.key,
+required this.pair,
+});
 
-  final WordPair pair;
+final WordPair pair;
 
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
+@override
+Widget build(BuildContext context) {
+final theme = Theme.of(context);
+final style = theme.textTheme.headlineLarge!.copyWith( //se accede a la fuente de texto
+color: theme.colorScheme.onPrimary,
+);
 
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
-      ),
-    );
-  }
+
+return Card( //widget nativo que permite dejar una carta (cuadrito) para poner un widget hijo
+color: theme.colorScheme.primary, //se elige el color de card
+child: Padding( //crea un nuevo widget donde la palabra aleatora de toma current, adquiere una carta en el fondo para mejorar la presentación
+padding: const EdgeInsets.all(20), // centra el texto del cuadro carta o dependiendo del aldo que queremos centrar
+// child: Text(pair.asUpperCase), //widget hijo que ewl paddin pide para ponert algo, puede ser un botton o en ese caso, el texto que viene desde el BigCard y es una palabra aleatoria
+child: Text(
+pair.asUpperCase,
+style: style,
+semanticsLabel: "${pair.first} ${pair.second}",
+)
+),
+);
 }
-
-// ...
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
 }
